57e479bbdfb7ed4da4bc861f32c321edffdc536d
Talisson Oliveira <talissonlux@outlook.com>
2021-08-25 12:30:06 -0300

    Created GithubCorner and Footer

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
-import { coverage } from 'browserslist'
 import styled from 'styled-components'
 import db from '../db.json'
+import Widget from '../src/components/Widget'
+import GitHubCorner from '../src/components/GithubCorner'
+import QuizBackground from '../src/components/QuizBackground'
+import Footer from '../src/components/Footer'
 
 const BackgroundImage = styled.div`
     background-image: url(${db.bg});
@@ -19,71 +22,21 @@ export const QuizContainer = styled.div`
   }
 `
 
-const Widget = styled.div`
-  margin-top: 24px;
-  margin-bottom: 24px;
-  border: 1px solid #4CAF50;
-  background-color: #1C1814;
-  border-radius: 4px;
-  overflow: hidden;
-
-  h1, h2, h3 {
-    font-style: 16px;
-    font-weight: 700;
-    line-height: 1;
-    margin-bottom: 0;
-  }
-
-  p {
-    font-size: 14px;
-    font-weight: 400;
-    line-height: 1;
-  }
-`
-
-Widget.Header = styled.header`
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  padding: 18px 32px;
-  background-color: ${({ theme }) => theme.colors.primary};
-
-  * {
-    margin: 0;
-  }
-`
-
-Widget.Content = styled.div`
-  padding: 24px 32px 32px 32px;
-
-  & > *:first-child {
-    margin-top: 0;
-  }
-
-  & > *:last-child {
-    margin-bottom: 0;
-  }
-
-  ul {
-    list-style: none;
-    padding: 0;
-  }
-`
-
 export default function Home() {
   return (
-    <BackgroundImage>
+    <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
         <Widget>
+          <Widget.Header>
+            <h1>Marvel Studios</h1>
+          </Widget.Header>
           <Widget.Content>
-            <Widget.Header>
-              <h1>Marvel Studios</h1>
-            </Widget.Header>
-
             <p>Teste os seus conhecimentos sobre o universo Marvel!</p>
           </Widget.Content>
         </Widget>
+        <Footer />
       </QuizContainer>
-    </BackgroundImage>
+      <GitHubCorner projectUrl="https://github.com/TalissonOliveira" />
+    </QuizBackground>
   )
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,42 @@
+import styled from 'styled-components'
+
+const FooterWrapper = styled.footer`
+  background-color: #00000070;
+  padding: 20px;
+  display: flex;
+  align-items: center;
+  border-radius: 4px; 
+  img {
+    width: 58px;
+    margin-right: 23px;
+  }
+  a {
+    color: white;
+    text-decoration: none;
+    transition: .3s;
+    &:hover,
+    &:focus {
+      opacity: .5;
+    }
+    span {
+      text-decoration: underline;
+    }
+  }
+`
+
+export default function Footer(props) {
+  return (
+    <FooterWrapper {...props}>
+      <a>
+        <img src="https://octodex.github.com/images/spidertocat.png" alt="Logo GitHub" />
+      </a>
+      <p>
+        Visite o projeto no
+        {' '}
+        <a href="https://github.com/TalissonOliveira/quiz" target="_blank">
+          <span>GitHub</span>
+        </a>
+      </p>
+    </FooterWrapper>
+  )
+}
